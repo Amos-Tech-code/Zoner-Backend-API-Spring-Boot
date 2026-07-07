a2c4235189f981fb93a56c40cf5b4d89a9ffedce
Amos-Tech-code <amosk5132@gmail.com>
2026-07-08 00:37:07 +0300

    Firebase Notification setup.

--- src/main/java/com/amos_tech_code/zoner/notifications/config/FirebaseConfig.java
+++ src/main/java/com/amos_tech_code/zoner/notifications/config/FirebaseConfig.java
@@ -1,7 +1,14 @@
 package com.amos_tech_code.zoner.notifications.config;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
 
 @Configuration
 public class FirebaseConfig {
